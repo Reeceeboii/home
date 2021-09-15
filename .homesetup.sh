86f437a26268e54311057c5b6a5332153387b1bc
Reece Mercer <reecemercer981@gmail.com>
2021-09-15 06:32:43 +0100

    Setup script

--- .homesetup.sh
+++ .homesetup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# if authenticated, pull and clean up, else I'm dumb and haven't generated SSH keys on the machine yet
-if git@github.com; then
-  cd
-  git init
-  git remote add origin git@github.com:Reeceeboii/home.git
-  git pull origin master
-  rm ./LICENSE README.md
-else
-  echo "Git SSH not configured"
+# try SSH authing for private repo access, warn if this does not work
+if ! git@github.com; then
+  echo "SSH not in place, auth will not work for private repos"
 fi
+
+git init
+git remote add origin git@github.com:Reeceeboii/home.git
+git pull origin master
+# clean up misc files (inc. this script itself)
+rm ./LICENSE README.md ./.homesetup.sh
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# home
-Welcome ~
+# Home
+
+curl the raw contents of setup script and pipe into bash, voila
+
+### `~$ curl https://raw.githubusercontent.com/Reeceeboii/home/master/.homesetup.sh | bash `
