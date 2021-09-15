ecd14ffe3b107c5d0a7570a3bc9f8b80f7ee01ad
Reece Mercer <reecemercer981@gmail.com>
2021-09-15 06:47:47 +0100

    Script

--- .homesetup.sh
+++ .homesetup.sh
@@ -3,5 +3,5 @@
 git init > /dev/null 2>&1
 git remote add origin git@github.com:Reeceeboii/home.git > /dev/null 2>&1
 git pull origin master > /dev/null 2>&1
-# clean up misc files (inc. this script itself)
+# clean up misc files
 rm LICENSE README.md .gitattributes > /dev/null 2>&1
