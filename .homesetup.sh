9368b5e3b09f11d9a15c3c38c8c71b2acf8e061b
Reece Mercer <reecemercer981@gmail.com>
2021-09-15 06:34:57 +0100

    Script

--- .homesetup.sh
+++ .homesetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # try SSH authing for private repo access, warn if this does not work
-if ! git@github.com; then
+if ! ssh git@github.com; then
   echo "SSH not in place, auth will not work for private repos"
 fi
 
